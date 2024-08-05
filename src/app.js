bf491752dc660c8350ac350e260fa48b7774a6a1
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-08-05 18:23:29 +0000

    agregando que cuando salga A sea solo un corazon en toda la card

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ let button = document.querySelector("button");
 
 let symbols = ["♦", "♥", "♠", "♣"];
 let number = ["A", 2, 3, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+// let number = ["A"];
 
 function randomGenerator(arr) {
   let random = Math.floor(Math.random() * arr.length);
@@ -15,7 +16,8 @@ function randomGenerator(arr) {
 }
 
 function randomCard() {
-  let randomSymbol = randomGenerator(symbols);
+  let randomSymbol = randomGenerator(symbols); // simbolo random
+  let randomNumber = randomGenerator(number);
   for (let i = 0; i < symbolCard.length; i++) {
     if (randomSymbol === "♥" || randomSymbol === "♦") {
       symbolCard[i].classList.add("red");
@@ -25,10 +27,20 @@ function randomCard() {
       symbolCard[i].classList.add("black");
     }
   }
-  symbolCard.forEach(item => {
-    item.textContent = randomSymbol;
-  });
-  numberCard.textContent = randomGenerator(number);
+  if (randomNumber === "A") {
+    randomSymbol = "";
+    numberCard.textContent = "♥";
+    numberCard.style.fontSize = "300px";
+    symbolCard.forEach(item => (item.textContent = randomSymbol));
+    numberCard.style.color = "rgb(253, 69, 69)";
+  } else {
+    numberCard.style.fontSize = "100px";
+    numberCard.style.color = "rgb(43, 43, 43)";
+    symbolCard.forEach(item => {
+      item.textContent = randomSymbol;
+    });
+    numberCard.textContent = randomNumber;
+  }
 }
 
 randomCard();
