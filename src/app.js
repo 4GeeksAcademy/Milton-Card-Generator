7baca00ca757a7b2168e599a80bba435683f384a
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-08-05 17:09:46 +0000

    creando una carta de manera random y dando colores al palo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let symbolCard = document.querySelectorAll(".symbol");
+let numberCard = document.querySelector(".number");
+let button = document.querySelector("button");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let symbols = ["♦", "♥", "♠", "♣"];
+let number = ["A", 2, 3, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+function randomGenerator(arr) {
+  let random = Math.floor(Math.random() * arr.length);
+  return arr[random];
+}
+
+function randomCard() {
+  let randomSymbol = randomGenerator(symbols);
+  for (let i = 0; i < symbolCard.length; i++) {
+    if (randomSymbol === "♥" || randomSymbol === "♦") {
+      symbolCard[i].classList.add("red");
+      symbolCard[i].classList.remove("black");
+    } else {
+      symbolCard[i].classList.remove("red");
+      symbolCard[i].classList.add("black");
+    }
+  }
+  symbolCard.forEach(item => {
+    item.textContent = randomSymbol;
+  });
+  numberCard.textContent = randomGenerator(number);
+}
+
+randomCard();
+
+button.addEventListener("click", () => {
+  randomCard();
+});
